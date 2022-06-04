771cb1e85309d45e108f6d0c7952fcde217aab2d
pierreilki <pvillard@ilki.fr>
2022-06-04 10:19:24 +0000

    improve deployment method

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
+#!/bin/bash
+
+if [ "${NEXUS_DOMAIN_NAME}" = "" ];then
+        read -p "nexus domain name: " NEXUS_DOMAIN_NAME
+fi
+if [ "${JENKINS_DOMAIN_NAME}" = "" ];then
+        read -p "jenkins domain name: " JENKINS_DOMAIN_NAME
+fi
+if [ "${GITLAB_DOMAIN_NAME}" = "" ];then
+        read -p "gitlab domain name: " GITLAB_DOMAIN_NAME
+fi
+
+
+NEXUS_DOMAIN_NAME=${NEXUS_DOMAIN_NAME} JENKINS_DOMAIN_NAME=${JENKINS_DOMAIN_NAME} GITLAB_DOMAIN_NAME=${GITLAB_DOMAIN_NAME} DOLLAR='$' envsubst < nginx/nginx.conf.template > nginx/nginx.conf
+
+
 sudo mkdir -p ./ssl/gitlab
 openssl genrsa -out ./ssl/gitlab/tls.key 2048
-openssl req -new -x509 -key ./ssl/gitlab/tls.key -out ./ssl/gitlab/tls.cert -days 360 -subj /CN=gitlab.ilkilab.io
+openssl req -new -x509 -key ./ssl/gitlab/tls.key -out ./ssl/gitlab/tls.cert -days 360 -subj /CN=${GITLAB_DOMAIN_NAME}
 
 sudo mkdir -p ./ssl/nexus
 openssl genrsa -out ./ssl/nexus/tls.key 2048
-openssl req -new -x509 -key ./ssl/nexus/tls.key -out ./ssl/nexus/tls.cert -days 360 -subj /CN=nexus.ilkilab.io
+openssl req -new -x509 -key ./ssl/nexus/tls.key -out ./ssl/nexus/tls.cert -days 360 -subj /CN=${NEXUS_DOMAIN_NAME}
 
 sudo mkdir -p ./ssl/jenkins
 openssl genrsa -out ./ssl/jenkins/tls.key 2048
-openssl req -new -x509 -key ./ssl/jenkins/tls.key -out ./ssl/jenkins/tls.cert -days 360 -subj /CN=jenkins.ilkilab.io
+openssl req -new -x509 -key ./ssl/jenkins/tls.key -out ./ssl/jenkins/tls.cert -days 360 -subj /CN=${JENKINS_DOMAIN_NAME}
 
-docker-compose up -d
+GITLAB_EXTERNAL_URL=https://${GITLAB_DOMAIN_NAME} docker-compose up -d
--- destroy.sh
+++ destroy.sh
@@ -2,3 +2,4 @@
 
 sudo docker-compose down -v --rmi all --remove-orphans
 rm -rf ./ssl
+rm -rf ./nginx/nginx.conf
